71f4d719fd40985c543990ce7c53b5f5dad8e052
turhan287 <77552541+turhan287@users.noreply.github.com>
2021-01-23 20:15:49 +0300

    Add files via upload

---
+++ MyList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenerecisIntro
+{
+    class MyDict<T>
+    {
+        T[] items;
+        public MyDict() //constructor oluşturduk ctor tab tab yaz. bir class new lendiğinde çalışan satıra const denir.
+        {                // MyList class açılınca otomatik const da çalışır.
+            items = new T[0];// 0 elemanlı bir dizi oluşturduk.
+
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = items;  // dizideki elemanlar kaybolmasın diye geçici dizi olşuturduk referansı temparraye atadık.
+            items = new T[items.Length+1]; // items dizisinin eleman sayısını 1 artırdık dinamik dizi oluşturduk.
+            for (int i = 0; i < tempArray.Length; i++)// items dizisi 1 arttı ve eskiden temparraye atılan değerleri geri alındı.
+            {
+                items[i] = tempArray[i];
+            }
+            items[items.Length - 1] = item;     //Add classta gelen item son elemana atandı.   
+        }
+     
+    
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenerecisIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            MyDict<string> isimler = new MyDict<string>();
+            isimler.Add("Engin");
+            
+        }
+    }
+}
